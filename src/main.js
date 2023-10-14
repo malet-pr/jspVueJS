65090a6135e7970aa2485034ba8c14fb48a650c5
malet-pr <maletq.pr@gmail.com>
2023-10-14 11:50:54 -0300

    remove imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import "primeicons/primeicons.css";
+//import "primeicons/primeicons.css";
 import "/node_modules/primeflex/primeflex.css";
-//import "primevue/resources/themes/bootstrap4-dark-purple/theme.css";
 import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 //import '@/assets/theme.css';
 
