e435fed8f1046e100a55fa06568717ab0a910bc3
malet-pr <maletq.pr@gmail.com>
2023-10-14 11:44:36 -0300

    remove router for build

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
-import "primeicons/primeicons.css";
+//import "primeicons/primeicons.css";
 import "/node_modules/primeflex/primeflex.css";
-//import "primevue/resources/themes/bootstrap4-dark-purple/theme.css";
-//import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
-import '@/assets/theme.css';
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import mitt from 'mitt';
 import App from './App.vue';
-import router from './router';
 
 const emitter = mitt()
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(PrimeVue)
 app.config.globalProperties.emitter = emitter
 
